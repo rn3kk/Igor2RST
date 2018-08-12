dd2bae53682c3fff69c33602c1e935f86862ca35
nbanshchikov <nbanshchikov@inibitec.com>
2018-08-12 13:28:13 +0300

    -просчет CRC для пакета который вызывал подозрение

--- crc16/main.cpp
+++ crc16/main.cpp
@@ -13,41 +13,39 @@ int bits(unsigned __int16 w)
   return bits;
 }
 
-void crc(unsigned __int16* arr)
+void crc(int numByte, quint16* arr)
 {
-  unsigned __int16 w0, w1, w2, w3, w4;
-  w0= 0xffff;
-  for(int i=0; i<14; ++i) //i-> w4
+  qDebug() << "Packet";
+  for(int i=0; i< numByte; ++i)
   {
+    qDebug() << "index=" << i<< " "<< QString::number(arr[i],16);
+  }
+  quint16 w0, w1, w2, w3, w4;
+  w0= 0xffff;
+  for(int i=0; i<numByte; ++i) //i-> w4
+  {
+    //qDebug() << "W0=" << QString::number(w0, 16) << " Istep:="<< QString::number(i);
     w1 = arr[i];
     w2 = 0x00ff;
 
     w1 = w1 & w2;
     w0 = w0 ^ w1;
 
-    bool b(false);
-    for(int j = 9; j!=0; --j )//j->w3
+    for(int j = 1; j<9; ++j )//j->w3
     {
-      b = true;
-       unsigned __int16 c = (w0 >> 0) & 1U;
-       int bitSize=bits(w0);
-       w0 = w0 >> 1;
-       if(c) //в старший бит w0 пишем с
-         w0 |= 1 << (bitSize - 1);
-       else
-         w0 &= ~(1 << (bitSize - 1));
-
-       c = (w0 >> 0) & 1U; //lowBit to c
-       if(c==1)
-       {
-         w2 = 0x8005;
-         w0 = w0 ^ w2;
-       }
+      //qDebug() << "      W0=" << QString::number(w0, 16) << " Jstep:="<< QString::number(j);
+      quint16 c = (w0 >> 0) & 1U; //lowBit to c
+      w0 = w0 >> 1;
+      w0 = w0 & (~(1 << 15));
+      if(c==1)
+      {
+        w2 = 0x8005;
+        w0 = w0 ^ w2;
+      }
     }
-
-    if(b)break;
   }
-   w0 = ((w0 << 8) & 0xff00) | ((w0 >> 8) & 0x00ff);
+  w0 = ((w0 << 8) & 0xff00) | ((w0 >> 8) & 0x00ff);
+  qDebug() << "CRC=" << QString::number(w0, 16);
 }
 
 quint16 crc(const QByteArray &data, int numByte)
@@ -55,6 +53,7 @@ quint16 crc(const QByteArray &data, int numByte)
   quint16 w0 = 0xffff, w1, w2, w3, w4 = 0;
   do
   {
+    qDebug() << "AAA W0=" << QString::number(w0, 16) << " Istep:="<< QString::number(w4);
     w1 = data[w4++];
     w2 = 0x00ff;
     w1 &= w2;
@@ -62,6 +61,7 @@ quint16 crc(const QByteArray &data, int numByte)
     w3 = 0x0009;
     while(--w3)
     {
+      //qDebug() << "      W0=" << QString::number(w0, 16) << " step:="<< QString::number(w3);
       bool c = w0 & 0x0001;
       w0 = (w0 >> 1) & 0x7fff;
       if(c)
@@ -72,59 +72,65 @@ quint16 crc(const QByteArray &data, int numByte)
     }
   }
   while(--numByte);
+  qDebug() << "AAAbefore W0=" << QString::number(w0, 16);
   w0 = ((w0 << 8) & 0xff00) | ((w0 >> 8) & 0x00ff);
+  qDebug() << "AAAafter W0=" << QString::number(w0, 16);
   return w0;
 }
 
-//"21090c090014ffffffffffffffffffffffff2020202020202000a8d7"
-//05.08.2018 00:12:59.359 [DEBUG  ] packet crc  "ffffffffffffa8d7"  calculate crc  "ffffffffffffa8d7"
-
+//210c00000005000a000103a7032602a402210981
+//У меня CRC получается 0x5971 (ваша 0x8109)
 int main(int argc, char *argv[])
 {
   QCoreApplication a(argc, argv);
 
-  QByteArray data = QByteArray::fromHex("2111000a41522d343376322e3035ede2");
-  quint16 result = crc(data, 14);
-
-
-/*  const char dataPtr[] = {
-      '\x21', '\x11', '\x00', '\x0a', '\x41', '\x52',
-                       '\x2d', '\x34', '\x33', '\x76', '\x32', '\x2e',
-                       '\x30', '\x35', '\xed', '\xe2'
-  //    '\x21', '\x09', '\x01', '\xBF', '\x00', '\x14', '\x00', '\x24', '\x20',
-//      '\xA8', '\x20', '\xA8', '\x02', '\xED', '\x02', '\xED', '\x00', '\x00',
-  //    '\x53', '\x4B', '\x4F', '\x52', '\x41', '\x4A', '\x41', '\x00', '\x9A', '\xB0'
-    };
-   data = QByteArray(dataPtr, 16);*/
-   qint16 crcC = crc(data, 16);
-
-
-//  QByteArray ar= QByteArrayLiteral("\x21\x11\x00\x0a\x041\x52\x2d\x34\x33\x76\x32\x2e\x30\x35");
-  quint16* arr = new quint16[14];
+  int count = 6;
+  quint16* arr = new quint16[count];
   arr[0] = 0x21;
-  arr[1] = 0x11;
+  arr[1] = 0x0B;
   arr[2] = 0x00;
-  arr[3] = 0x0a;
-  arr[4] = 0x41;
-  arr[5] = 0x52;
-  arr[6] = 0x2d;
-  arr[7] = 0x34;
-  arr[8] = 0x33;
-  arr[9] = 0x76;
-  arr[10] = 0x32;
-  arr[11] = 0x2e;
-  arr[12] = 0x30;
-  arr[13] = 0x35;
+  arr[3] = 0x00;
+  arr[4] = 0x00;
+  arr[5] = 0x0A;
+ // crc(count, arr);
 
-  QDataStream stream;
-  for(int i=0; i<14; ++i)
+  QByteArray array;
+  for(int i=0; i<count; ++i)
   {
-    qDebug() << "i:" << i << " " << QString::number(arr[i], 16);
+    array.append(qint16(arr[i]));
   }
+  //crc(array, count);
 
- // crc(arr);
 
-  int count = 14;
+  int count2=18;
+  quint16* arr2 = new quint16[count2];
+  arr2[0] = 0x21;
+  arr2[1] = 0x0c;
+  arr2[2] = 0x00;
+  arr2[3] = 0x00;
+  arr2[4] = 0x00;
+  arr2[5] = 0x05;
+  arr2[6] = 0x00;
+  arr2[7] = 0x0a;
+  arr2[8] = 0x00;
+  arr2[9] = 0x01;
+  arr2[10] = 0x03;
+  arr2[11] = 0xa7;
+  arr2[12] = 0x03;
+  arr2[13] = 0x26;
+  arr2[14] = 0x02;
+  arr2[15] = 0xa4;
+  arr2[16] = 0x02;
+  arr2[17] = 0x21;
+
+  crc(count2, arr2);
+  array.clear();
+  for(int i=0; i< count2; ++i)
+  {
+    array.append(qint16(arr2[i]));
+  }
+  crc(array, count2);
+
   quint16 w0,w1,w2,w3,w4;
 
   w0= 0xffff;
@@ -158,19 +164,14 @@ CRC00:
     goto CRC_BYTE_RETURN;
 
   bool c = (w0 >> 0) & 1U;
-  int bitSize=bits(w0);
   w0 = w0 >> 1; //shift
-  w0 = (w0 >> 1) & 0x7fff;
-//  if(c) //в старший бит w0 пишем с
-//    w0 |= 1 << (bitSize - 1);
-//  else
-//    w0 &= ~(1 << (bitSize - 1));
 
   if(c!=1)
     goto CRC00;
 
   w2 = 0x8005;
   w0 = w0 ^ w2;
+  qDebug() <<" CRC00 end. w0=" << QString::number(w0, 16);
   goto CRC00;
 
 END:
